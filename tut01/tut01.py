7a9cf3962edbe8f75c0b552807f4f9ad1d0ab13a
Jatin <jatin.k0jatin@gmail.com>
2022-09-14 17:52:52 +0530

    Commit 6
    Optimized the time complexity and reduced the length of code.

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,18 +1,15 @@
 #Import the necessary modules
 #I will be reading file through using csv, pandas both
-import csv
-import os
-from csv import writer, reader
-import pandas as pd
+import pandas
 
 #As pandas raise settingWithCopyWarning,
 #As it occur due to chain assignment,
 #so to avoid its display in terminal, i used options attribute which is used to configure and prevent it from displaying error and exception
-pd.options.mode.chained_assignment = None
+pandas.options.mode.chained_assignment = None
 
 #Octant function takes three coordinates as parameter and return the octant to which these belong
 def octant(x,y,z):
-    if z>=0.0:
+    if z>=0.000000000:
         if(x>=0.000000000 and y>=0.000000000  ): return 1
         elif(x<0.000000000  and y>=0.000000000 ): return 2
         elif(x<0.000000000  and y<0.000000000 ): return 3
@@ -24,48 +21,37 @@ def octant(x,y,z):
         elif(x>=0.000000000  and y<0.000000000 ): return -4
 
 def octact_identification(mod=5000):
-    f1 = open('octant_input.csv', 'r')
-    f2 = open('octant_output.csv', 'a')
+    f1 = pandas.read_csv("octant_input.csv")
+    f1.to_csv("octant_output.csv", index=False)
+    file2 = pandas.read_csv("octant_output.csv")
     #1counting the number of rows in csv file and computing the average of each U, V, W
-    with f1:
-        avg_U, avg_V,avg_W=0.0,0.0,0.0 #first store the sum of all observation
-        row__=0 #variable to keep count of number of rows
-        f2.writelines(f1.readlines()) #writing the first 4 columns in octant_output.csv
-        f1.seek(0) #move pointer in file to beginning of file
-        for row_number, row in enumerate(f1.readlines()):
-            #enumerate return a key: number of row and pair:row in string form
-            if row_number == 0:
-                continue #ignoring first row, it contains headers
-            else:
-                row__ += 1
-                data = row.split(',')
-                avg_U = round(float(data[1]),9)+avg_U
-                avg_V = round(float(data[2]),9)+avg_V
-                avg_W = round(float(data[3]),9)+avg_W
-        #computing average which is total sum of observations / number of observation
-        avg_U= round(1.0*avg_U/row__,9) #round to 9 decimal places
-        avg_V= round(1.0*avg_V/row__,9)
-        avg_W= round(1.0*avg_W/row__,9)
-    f1.close(), f2.close() #close both file handlers for now
 
-    #2 Opening file using pandas
-    file1 = pd.read_csv("octant_input.csv")
-    file2 = pd.read_csv("octant_output.csv")
+    avg_U, avg_V,avg_W=0.0,0.0,0.0 #first store the sum of all observation
+    #shape attribute of pandas return number of rows and columns
+    row__, column__ = file2.shape #variable to keep count of number of rows
 
-    #inserting using insert(index to insert, value of column, value by which every cell will be filled)
+    avg_U = file2['U'].sum()
+    avg_V = file2['V'].sum()
+    avg_W = file2['W'].sum()
+    #computing average which is total sum of observations / number of observation
+    avg_U= round(1.0*avg_U/row__,9) #round to 9 decimal places
+    avg_V= round(1.0*avg_V/row__,9)
+    avg_W= round(1.0*avg_W/row__,9)
+
+    #2inserting using insert(position to insert, value of column, value by which every cell will be filled)
     #creating seven column and inserting blank value
-    file2.insert(len(file2.columns), 'U avg', '')
-    file2.insert(len(file2.columns), 'V avg', '')
-    file2.insert(len(file2.columns), 'W avg', '')
+    file2.insert(len(file2.columns), 'U Avg', '')
+    file2.insert(len(file2.columns), 'V Avg', '')
+    file2.insert(len(file2.columns), 'W Avg', '')
     file2.insert(len(file2.columns), 'U\'=U - U avg', '')
     file2.insert(len(file2.columns), 'V\'=V - V avg', '')
     file2.insert(len(file2.columns), 'W\'=W - W avg', '')
     file2.insert(len(file2.columns), 'Octant', '')
 
     #Rounding the column value upto 9 decimal values
-    file2['U avg'][0]=round(avg_U,9)
-    file2['V avg'][0]=round(avg_V,9)
-    file2['W avg'][0]=round(avg_W,9)
+    file2['U Avg'][0]=round(avg_U,9)
+    file2['V Avg'][0]=round(avg_V,9)
+    file2['W Avg'][0]=round(avg_W,9)
 
     #creating a dictionary to keep count of the eight octant
     _hash = {'1':0, '-1':0, '2':0, '-2':0, '3':0, '-3':0, '4':0, '-4':0}
@@ -111,48 +97,40 @@ def octact_identification(mod=5000):
     file2['-4'][0]=_hash['-4']
 
     #6 Generating the values that will be bound of intervals like 0,5000,10000 for mod = 5000 and storing them in list
-    list_value=[]
-    for i in range(0, row__, mod): #start with zero till number of rows and increment by mod
-        list_value.append(i)
-    else: #at last, the smallest multiple of mod which is greater than number of rows; else will execute at last, able to compute last value of bound
-        list_value.append(i+mod)
-
-    #in the upcoming for loop, start position is always incremented by 1
-    #so making 0 as -1, -1+1 will again be zero
-    list_value.pop(0)
-    list_value.insert(0, -1.0000)
+    Bounds_mod_range=[]
+    for count in range(0, row__, mod): #start with zero till number of rows and increment by mod
+        Bounds_mod_range.append(count)
+    Bounds_mod_range.append(row__+1)
 
     #Again making a dictionary, that will have for each octant all the intervals in form of list
-    #Example {'1':[[0,5000,0], [5001,10000,0], .....], '-1':[[0,5000,0], [5001,10000,0], .....], ..........}
+    #Example {'1':[[0,4999,0], [5000,9999,0], .....], '-1':[[0,4999,0], [5000,9999,0], .....], ..........}
     #        'octant_value' : [[start_index, end_index, count of value]]
-    list_final = {'1':[], '-1':[], '2':[], '-2':[], '3':[], '-3':[], '4':[], '-4':[]}
-    for i in range(len(list_value)-1):
-        for j in list_final:
-            list_final[j].append([list_value[i]+1,list_value[i+1],0])
+    Count_range_wise = {'1':[], '-1':[], '2':[], '-2':[], '3':[], '-3':[], '4':[], '-4':[]}
+    for index in range(len(Bounds_mod_range)-1):
+        for count in Count_range_wise:
+            Count_range_wise[count].append([Bounds_mod_range[index],Bounds_mod_range[index+1]-1,0])
 
     #7 For each mod range of each octant, count number of octant in it
     for index, rows in file2.iterrows():
-        for i in list_final[str(file2['Octant'][index])]:
-            if index>=i[0] and index<=i[1]:
-                #if lies in this interval, increment the count
-                i[2]+=1
-                #we don't need to look furthur
-                break
+        # print(index)
+        for idx in Count_range_wise[str(file2['Octant'][index])]:
+                if index>=idx[0] and index<idx[1]:
+                    #if lies in this interval, increment the count
+                    idx[2]+=1
+                    #we don't need to look furthur
+                    break
 
     #8 Adding mod range labels in octant_output.csv file
-    for index in range(len(list_value)-1):
+    for count in range(len(Bounds_mod_range)-1):
         #index+2 as they will start to fill from 2nd row in Octant ID column
         #will insert the range as an string
-        if(list_final['1'][index][0]==0.0):
-            file2['Octant ID'][index+2]='0.0000'+'-'+str(list_final['1'][index][1])
-        else :
-            file2['Octant ID'][index+2]=str(list_final['1'][index][0])+'-'+str(list_final['1'][index][1])
+        file2['Octant ID'][count+2]=str(Count_range_wise['1'][count][0])+'-'+str(Count_range_wise['1'][count][1])
 
     #9 Filling count of each octant in each mod range into csv file
-    for key, value in list_final.items(): #iterating through dictionary
-        for i in range(len(list_value)-1): #number of ranges of mod will be constant
-            file2[str(key)][i+2]=str(list_final[key][i][2])
-            #<file_handler>['<column_for_octant>'][row number] = string of list_final[key is octant][serial number of mod range][count of values]
+    for key, value in Count_range_wise.items(): #iterating through dictionary
+        for index in range(len(Bounds_mod_range)-1): #number of ranges of mod will be constant
+            file2[str(key)][index+2]=str(Count_range_wise[key][index][2])
+            #<file_handler>['<column_for_octant>'][row number] = string of Count_range_wise[key is octant][serial number of mod range][count of values]
     #10 Saving all changes to output file
     file2.to_csv("octant_output.csv", index=False)
 
@@ -167,5 +145,5 @@ else:
     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
 #taking user input for mod and calling octact _identification() function
-mod = int(input("Enter the mod value:"))
+mod = 5000
 octact_identification(mod)
