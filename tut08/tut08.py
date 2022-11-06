1f8889f3de14a497fa4b77521fa46720ab4267c3
Jatin <jatin.k0jatin@gmail.com>
2022-11-06 14:13:37 +0530

    Commit 2
    Creating the list and readings the file.

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -1,11 +1,21 @@
-
+import re
 from datetime import datetime
 start_time = datetime.now()
 
 #Help
 def scorecard():
-	pass
+	#A dictionary having name of each player and short name used in commmentary
+	name = {'Bhuvneshwar':'Bhuvneshwar Kumar','Rahul':'KL Rahul', 'Rizwan':'Mohammad Rizwan(w)', 'Babar Azam':'Babar Azam(c)', 'Arshdeep Singh':'Arshdeep Singh', 'Fakhar Zaman':'Fakhar Zaman', 'Hardik Pandya':'Hardik Pandya', 'Avesh Khan':'Avesh Khan', 'Iftikhar Ahmed':'Iftikhar Ahmed', 'Chahal':'Yuzvendra Chahal', 'Jadeja':'Ravindra Jadeja', 'Khushdil':'Khushdil Shah', 'Shadab Khan':'Shadab Khan', 'Asif Ali':'Asif Ali', 'Mohammad Nawaz':'Mohammad Nawaz', 'Haris Rauf':'Haris Rauf', 'Naseem Shah':'Naseem Shah', 'Dahani':'Shahnawaz Dahani', 'Rohit':'Rohit Sharma(c)', 'Kohli':'Virat Kohli', 'Suryakumar Yadav':' Suryakumar Yadav', 'Karthik':'Dinesh Karthik(w)'}
+	#List of names of players of Team India
+	TeamIndia =[ 'Rohit','Rahul' ,'Kohli','Suryakumar Yadav', 'Karthik', 'Hardik Pandya', 'Jadeja', 'Bhuvneshwar', 'Avesh Khan', 'Chahal', 'Arshdeep Singh']
+	TeamPak = ['Babar Azam', 'Rizwan', 'Fakhar Zaman', 'Iftikhar Ahmed', 'Khushdil', 'Asif Ali', 'Shadab Khan', 'Mohammad Nawaz', 'Naseem Shah', 'Haris Rauf', 'Dahani']
+	#Opening the file
+	f = open('Scorecard.txt', 'w')
+	#Reading the innings file
+	f1 = open('pak_inns1.txt', 'r')
+	f2 = open('india_inns2.txt', 'r')
 
+	
 
 ###Code
 
