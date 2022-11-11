ef776120afa1af720dd9cca90c288e548f486eae
Jatin <jatin.k0jatin@gmail.com>
2022-11-11 17:50:40 +0530

    Commit 3
    Added code for Tab of Single File Processing.

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -38,10 +38,7 @@ else:
 		
 		
 		
-		#24 Opening in openpyxl module to do formating
-		# Method to save in specific folder: os.path.join method, for joining one or more path components.
-		# a = 1.0.xlsx
-		# to get only "1.0" split by ".xlsx" and take first part which is the name
+		
 
 def proj_octant_gui():
 	#Printing title on screen
@@ -57,6 +54,61 @@ def proj_octant_gui():
 
 	#st.tabs(<list of name of string>) is used to create tabs
 	t1, t2 = st.tabs(["Single File", "Multiple system"])
+
+	#Defining tab t1
+	with t1:
+		#Text to be displayed
+		st.write("Single File Uploading")
+		st.text('1. Upload your file.')
+		st.text('2. Enter mod value.')
+		st.text('3 Click on Compute Button.')
+		st.write('*Not following the above steps for interacting will led to errors.')
+
+		#st.file_uploader(<text to be displayed>, type of file, key for this element) is use to take specific type file input from user
+		inputFile = st.file_uploader("Upload Excel Octant Input", type="xlsx", key="inputFile")
+
+		#taking input for mod value
+		#st.number_input is use to take number input, 'format' is use to specify type of number like decimal, integer
+		mod = st.number_input("Enter mod value:", format="%i", key="mod2")
+
+		#Creating a compute button
+		reg = st.button('COMPUTE',on_click = None)
+		#If button is clicked
+		if reg:
+			#In streamlit, on clicking a button
+			#The script is refreshed and all the run time values like here of mod, file input are lost
+			#using st.session_state is a dictionary type use to store values in run time
+			#When the script is refreshed due to button click
+
+			#This session state dictionary data is not lost and we can use these values in program
+
+
+			#Calling this function designed to process a single file
+			octant_analysis_For_single_file(st.session_state.inputFile, int(st.session_state.mod2))
+
+			#Taking current time in format 'Year-month-day-hour-minute-second'
+			t = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+			#generating name for output file using fstring
+			filename = st.session_state.inputFile.name.split(".xlsx")[0]+"_{0}_{1}.xlsx".format(st.session_state.mod2, t)
+
+			#opening Temp.xlsx file generated during octant_analysis_For_single_file()
+			with open("./Temp.xlsx", 'rb') as my_file:
+				#Providing option to download using download button of streamlit
+				#label is text for button
+				#data is the dataframe
+				#file_name is the name of downloading file
+				#mime is the type of file
+				st.download_button(label = 'Download Output', data = my_file, file_name = filename, mime = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+			
+			#Deleting the file from current directory
+			os.remove("./Temp.xlsx")  
+
+		#Clearing the session state as it could get clear now	 
+		for i in st.session_state.keys():
+			del i
+
+
 
 	
 
